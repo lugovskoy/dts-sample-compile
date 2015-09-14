f37c172aef31dccf77236385a87d45cbc5f12997
lugovskoy <lugovskoy@gmail.com>
2015-09-14 16:25:53 +0300

    Refactor task to use only __init__ file.

--- __init__.py
+++ __init__.py
@@ -4,35 +4,47 @@
 import os.path
 import subprocess
 
+__version__ = 1
+__arguments = [
+    {"name": "prefix", "type": "text", "title": "Install directory prefix (used for configure script)"},
+    {"name": "patch", "type": "file", "title": "Patch file"},
+    {"name": "strip", "type": "bool", "title": "Strip symbol tables from resulting binaries?"},
+    {"name": "flags", "type": "checkbox", "values": ["Wall", "g", "O2"], "title": "Specify compiler flags"},
+    {"name": "lib", "type": "radio", "values": ["libtcc", "libgcc"], "title": "Select library to use"}
+]
 
-class SubTask():
-    def __init__(self, output_dir, log):
-        self.__output_dir = output_dir
-        self.__log = log
-        self.__wd = os.path.dirname(os.path.realpath(__file__))
-        self.__init_done = False
+
+class Task(object):
+    @staticmethod
+    def setup(self, srcdir):
+        print "__setup__"
+        retcode = subprocess.call(['git', 'clone', 'https://github.com/lugovskoy/dts-sample-compile.git', srcdir])
+        if retcode != 0:
+            raise "Cannot setup git repo in {0}".format(srcdir)
+
+
+    def __init__(self):
         print "__init__"
+        self.__wd = os.path.dirname(os.path.realpath(__file__))
+        self.__tcc_dir = os.path.join(self.__wd, 'tcc')
+        retcode = subprocess.call(['git', 'clone', 'git://repo.or.cz/tinycc.git', tcc_dir])
+        if retcode != 0:
+            raise "Cannot initialize git repo in {0}".format(tcc_dir)
 
 
-    def is_initialized(self):
-        print "init", self.__init_done
-        return self.__init_done
+    def __call__(self, args, outdir, q):
+        print "__call__"
+        tcc_srcdir = self.__tcc_dir
+        tcc_outdir = os.path.join(outdir, 'opt')
+        os.chdir(tcc_srcdir)
+        retcode = subprocess.call(['./configure.sh', '--prefix={0}'.format(tcc_outdir)])
+        if retcode != 0:
+            raise "Cannot configure in {0}".format(tcc_srcdir)
+        retcode = subprocess.call(['make'])
+        if retcode != 0:
+            raise "Cannot make in {0}".format(tcc_srcdir)
+        retcode = subprocess.call(['make', 'install'])
+        if retcode != 0:
+            raise "Cannot install to {0}".format(tcc_outdir)
+        q.put({'tcc': os.path.join(tcc_outdir, 'bin')})
 
-
-    def initialize(self):
-        print "initialize"
-        self.__init_done = True
-        script = os.path.join(self.__wd, 'get_tcc.sh')
-        retcode = subprocess.call([script, self.__wd])
-        self.__init_done = retcode == 0
-
-
-    def is_enabled(self):
-        return True
-
-
-    def run(self, q, args):
-        print "run"
-        script = os.path.join(self.__wd, 'conf_and_make.sh')
-        retcode = subprocess.call([script, self.__wd, self.__output_dir, self.__log])
-        q.put({'tcc': os.path.join(self.__output_dir, 'tcc', 'bin')})
--- get_tcc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-WORKDIR=$1
-
-cd $WORKDIR || exit 1
-git clone git://repo.or.cz/tinycc.git tinycc || exit 1
