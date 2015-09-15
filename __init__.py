80084eafeba0b1f1393d4c122296457c2542fcac
lugovskoy <lugovskoy@gmail.com>
2015-09-15 19:28:43 +0300

    Remove exceptions, add return tcc value.

--- __init__.py
+++ __init__.py
@@ -20,9 +20,6 @@ class Task(object):
     def setup(srcdir):
         print "__setup__"
         retcode = subprocess.call(['git', 'clone', 'https://github.com/lugovskoy/dts-sample-compile.git', srcdir])
-        return
-        if retcode != 0:
-            raise Exception("Cannot setup git repo in {0}".format(srcdir))
 
 
     def __init__(self):
@@ -30,25 +27,16 @@ class Task(object):
         self.__wd = os.path.dirname(os.path.realpath(__file__))
         self.__tcc_dir = os.path.join(self.__wd, 'tcc')
         retcode = subprocess.call(['git', 'clone', 'git://repo.or.cz/tinycc.git', self.__tcc_dir])
-        return 
-        if retcode != 0:
-            raise Exception("Cannot initialize git repo in {0}".format(self.__tcc_dir))
 
 
-    def __call__(self, args, resdir, q):
+    def __call__(self, args, refs, resdir, q):
         print "__call__"
         tcc_srcdir = self.__tcc_dir
         tcc_resdir = os.path.join(resdir, 'opt')
-        print tcc_srcdir
         os.chdir(tcc_srcdir)
         retcode = subprocess.call(['./configure', '--prefix={0}'.format(tcc_resdir)])
-        if retcode != 0:
-            raise Exception("Cannot configure in {0}".format(tcc_srcdir))
         retcode = subprocess.call(['make'])
-        if retcode != 0:
-            raise Exception("Cannot make in {0}".format(tcc_srcdir))
         retcode = subprocess.call(['make', 'install'])
-        if retcode != 0:
-            raise Exception("Cannot install to {0}".format(tcc_resdir))
-        q.put({'tcc': os.path.join(tcc_resdir, 'bin')})
+        tcc = os.path.join(tcc_resdir, 'bin', 'tcc')
+        q.put({'tcc': tcc})
 
